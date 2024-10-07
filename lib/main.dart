d0bfe18ec05163e431ad8e5e8256be742ae99b3d
abdaziz04 <abdaziz1713@gmail.com>
2024-10-07 21:15:06 +0700

    Praktikum 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MainApp());
@@ -7,14 +8,46 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Expanded(
+            // Soal 1
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Soal 2
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text('Batu, Malang, Indonesia',
+                    style: TextStyle(color: Colors.grey[500])),
+              ],
+            ),
+          ),
+          // Soal 3
+          Icon(Icons.star, color: Colors.red[500]),
+        ],
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout : Abdul Aziz / 2241720088',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Column(
+          children: [
+            titleSection,
+          ],
         ),
       ),
     );
